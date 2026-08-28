4d1af1d1864bb7e39bdc1c8aba844701755b62fe
NescoPesto <pablobraun2003@gmail.com>
2026-08-28 22:22:52 +0200

    CamelCase

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
-local mod = RegisterMod("Nesco's Items", 1)
-local napster = Isaac.GetItemIdByName("Napster")
+local Mod = RegisterMod("Nesco's Items", 1)
+local Napster = Isaac.GetItemIdByName("Napster")
